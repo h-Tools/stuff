3d691ec215eee129aab30225283e4d2866caf585
zippee100 <rylancharnock6@gmail.com>
2023-07-25 14:54:06 +0100

    Update h_template.lua

--- place/h_template.lua
+++ place/h_template.lua
@@ -5,7 +5,7 @@
       {
         "name": "H",
         "sources": [
-          "https://github.com/zippee100/stuff/blob/main/template3.png?raw=true"
+          "https://github.com/zippee100/stuff/blob/main/place/template3.png?raw=true"
         ],
         "x": 50,
         "y": 1196
