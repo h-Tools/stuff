7d40d2057169c64a615d5fe3686317360d82073c
zippee100 <rylancharnock6@gmail.com>
2023-07-25 14:41:29 +0100

    Create h_template_info.lua

---
+++ h_template_info.lua
@@ -0,0 +1,16 @@
+{
+    "contact": "https://discord.gg/efKCpuA",
+    "notifications": "",
+    "templates": [
+      {
+        "name": "H",
+        "sources": [
+          "https://github.com/artillect/greenlattice-template/blob/main/lattice_template.png?raw=true"
+        ],
+        "x": 50,
+        "y": 1198
+      }
+    ],
+    "whitelist": [],
+    "blacklist": []
+  }
