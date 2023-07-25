3c3ebdaed68e090d1e042606d832e732586bdba6
zippee100 <rylancharnock6@gmail.com>
2023-07-25 14:58:09 +0100

    Create h.lua

---
+++ place/h.lua
@@ -0,0 +1,16 @@
+{
+    "contact": "https://discord.gg/efKCpuA",
+    "notifications": "",
+    "templates": [
+      {
+        "name": "H",
+        "sources": [
+          "https://github.com/zippee100/stuff/blob/main/place/templateh.png?raw=true"
+        ],
+        "x": 50,
+        "y": 1196
+      }
+    ],
+    "whitelist": [],
+    "blacklist": []
+  }
